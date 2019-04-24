337f0575730a36edfaf840b6ed80f413bd15a504
J. Austin Hughey <jah@jah.io>
2019-04-23 22:46:59 -0600

    explain reverse_words better

--- reverse_words.rb
+++ reverse_words.rb
@@ -17,8 +17,8 @@ backwards = "dog lazy the over jumps fox brown quick The"
 words = backwards.split(/\W+/)
 
 #
-# Given the array "words", without using any Ruby magic (e.g. "#reverse"),
-# invert its order.
+# This will eventually hold the contents of the (reversed) string,
+# with each word as an array element.
 # 
 
 sentence = []
@@ -28,6 +28,17 @@ sentence = []
 # 
 n = words.count
 
+#
+# Given the array "words", without using any Ruby magic (e.g. "#reverse"),
+# invert its order. This works by setting a variable equal to the total number
+# of words in the array, and running a "while" loop that checks if that number
+# is zero. Then, on every trip through the loop, we take the highest number index
+# of the words array, subtract one (since arrays are zero-indexed), push that
+# IN ORDER onto the end of the (empty) "sentence" array object, then reduce
+# the count of n by one. Eventually, on the last trip through, N will be zero
+# thus exiting the loop.
+# 
+
 while n != 0
 
   sentence << words[n - 1]
@@ -35,6 +46,10 @@ while n != 0
 end
 
 #
-# Now, output the sentence correctly
+# Now, "sentence" is also an array but with objects that look like this:
+#   ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+# 
+# We can easily use the #join method to put these together into a string
+# by using a simple space character as the one argument to #join.
 # 
 puts sentence.join(" ")
